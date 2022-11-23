c546abbcc367ee04a14da6623f1c53d69ae713f8
pairpilot <forgallaxytabfgt@gmail.com>
2022-11-23 21:20:43 +0900

    [FIX] unadded html

---
+++ bin/main/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO SAMPLE (id, title, content, writer, read_count, reg_date, password ) values (10,'sample article1','스프링부트와 쟝고를 옴팡지게 파서 잘먹고 잘 살아봅시다','관리자', 0,CURRENT_DATE ,1234);
+INSERT INTO SAMPLE (id, title, content, writer, read_count, reg_date, password ) values(11,'sample article2','스프링부트와 쟝고를 옴팡지게 파서 잘먹고 잘 살아봅시다','관리자',0, CURRENT_DATE,1234);
+INSERT INTO SAMPLE (id, title, content, writer, read_count, reg_date, password ) values(12,'sample article3','스프링부트와 쟝고를 옴팡지게 파서 잘먹고 잘 살아봅시다','관리자',0, CURRENT_DATE,1234);
+INSERT INTO SAMPLE (id, title, content, writer, read_count, reg_date, password ) values(13,'sample article4','스프링부트와 쟝고를 옴팡지게 파서 잘먹고 잘 살아봅시다','관리자',0, CURRENT_DATE,1234);
+INSERT INTO SAMPLE (id, title, content, writer, read_count , reg_date, password) values(14,'sample article5','스프링부트와 쟝고를 옴팡지게 파서 잘먹고 잘 살아봅시다','관리자', 0,CURRENT_DATE,1234);
+INSERT INTO SAMPLE (id, title, content, writer, read_count , reg_date, password) values(15,'sample article6','#자연명승지 #유원시설 #축제, 기타시설물 #코로나 시기 안전수칙 ','관리자',0, CURRENT_DATE,1234);
+INSERT INTO SAMPLE (id, title, content, writer, read_count, reg_date, password) values(16,'sample article7','스프링부트와 쟝고를 옴팡지게 파서 잘먹고 잘 살아봅시다','관리자',0,CURRENT_DATE,1234);
+INSERT INTO SAMPLE (id, title, content, writer, read_count, reg_date, password) values(17,'sample article8','스프링부트와 쟝고를 옴팡지게 파서 잘먹고 잘 살아봅시다','관리자',0,CURRENT_DATE,1234);
+INSERT INTO SAMPLE (id, title, content, writer, read_count, reg_date, password) values(111,'스프링부트와 장고로 블로그만들자','MSA 별거 아님, 원래 분산형 앱으로서 이미 존재하던건데, 최근 기술발전으로 보다 쉬워져서 풀린거임. 충분히 가능함','관리자',0,CURRENT_DATE,1234);
+
+
+INSERT INTO SUBJECT (id, name, explanation, writer, reg_date ) values (10,'통일된 개발환경','이게 우리가 협업할때 고민해야할 주제인거같아1','우리', CURRENT_DATE );
+INSERT INTO SUBJECT (id, name, explanation, writer, reg_date, updated_datetime ) values (11,'협업 방식','가령, 기록을 저장할 공통의 문서; 구글 독스, 회의방식; 구글미팅, 추가적으로 협업에 도움이 될만한 모든 것','우리', CURRENT_DATE ,CURRENT_DATE);
